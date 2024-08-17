3af3fb95bf45ca749a51509c8a1ce2dc7a811113
Marek Wiewiorski <marek@mwicat.com>
2024-08-17 19:19:22 +0200

    Remove unused function save_upload_file_tmp

--- backend/src/uploadsvc/util.py
+++ backend/src/uploadsvc/util.py
@@ -11,15 +11,3 @@ def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
             shutil.copyfileobj(upload_file.file, buffer)
     finally:
         upload_file.file.close()
-
-
-def save_upload_file_tmp(upload_file: UploadFile) -> Path:
-    try:
-        suffix = Path(upload_file.filename).suffix
-        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-            shutil.copyfileobj(upload_file.file, tmp)
-            tmp_path = Path(tmp.name)
-    finally:
-        upload_file.file.close()
-    return tmp_path
-
